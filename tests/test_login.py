3c7fdb68e293ff342659d0ace14c443ddbbdac51
Emer Gabrillo <emcleegab@gmail.com>
2026-07-03 09:14:11 +0800

    added base_url to login_page fixture to reduce clutter

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,37 +1,38 @@
+import pytest
 from pages.login_page import LoginPage
 
-def test_successful_login(driver, base_url):
+@pytest.fixture
+def login_page(driver, base_url):
+    """Automatically loads the base URL and returns the initialized Page Object."""
     driver.get(base_url)
-    
-    login_page = LoginPage(driver)
+    return LoginPage(driver)
+
+def test_successful_login(login_page, driver):
     login_page.enter_username("standard_user")
     login_page.enter_password("secret_sauce")
     login_page.click_login()  
     
-    # Verify successful login by checking for a specific element or URL change
-    assert "inventory.html" in driver.current_url  # Checks if the user is redirected to the inventory page after successful login
+    # Verify successful login by checking for URL change
+    assert "inventory.html" in driver.current_url
 
-def test_incorrect_username(driver, base_url):
-    driver.get(base_url)
-
-    login_page = LoginPage(driver)
+def test_incorrect_username(login_page):
     login_page.enter_username("wrong_user")
     login_page.enter_password("secret_sauce")
     login_page.click_login()
     
     # Verify incorrect username by checking for an error message
     error_warning = login_page.get_error_message()
-    assert error_warning is not None  # Checks if an error message is displayed
-    assert "Username and password do not match any user in this service" in error_warning  # Validates the specific error message for incorrect username
+    assert error_warning is not None
+    # Validates the specific error message for incorrect username
+    assert "Username and password do not match any user in this service" in error_warning  
 
-def test_incorrect_password(driver, base_url):
-    driver.get(base_url)
-    login_page = LoginPage(driver)
+def test_incorrect_password(login_page):
     login_page.enter_username("standard_user")
     login_page.enter_password("wrong_password")
     login_page.click_login()
     
     # Verify incorrect login by checking for an error message
     error_warning = login_page.get_error_message()
-    assert error_warning is not None  # Checks if an error message is displayed
-    assert "Username and password do not match any user in this service" in error_warning  # Validates the specific error message for incorrect paswsword
+    assert error_warning is not None
+     # Validates the specific error message for incorrect password
+    assert "Username and password do not match any user in this service" in error_warning 
